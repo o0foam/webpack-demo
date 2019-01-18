c920fdb1ac24024131ee4c4c9c214dacef43ccfa
foam <522653886@qq.com>
2019-01-18 20:28:15 +0800

    https://webpack.docschina.org/guides/output-management/

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component(){
     var element = document.createElement('div');
+    var btn = document.createElement('button');
     element.innerHTML = _.join(['hello', 'webpack', '']);
-    element.classList.add('hello');
 
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon)
-    console.log(Data);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
     return element;
 }
